2a2f15a43ba2504e567d6b681f288d1fa9888a23
kabi404 <javierparada73@outlook.com>
2018-09-12 09:35:47 +0200

    add basic container and rows

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-3 text-center bg-dark">
+              <h2 className="text-white my-5">kabi404</h2>
+            </div>
+            <div className="col-9 text-center">
+              <h2 className="my-5">Hola que hase</h2>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
