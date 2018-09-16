50fda70816adcea492c37cb21c0e604d84647a66
kabi404 <javierparada73@outlook.com>
2018-09-16 20:36:40 +0200

    add basic components

--- src/App.js
+++ src/App.js
@@ -3,27 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 import Nav from './components/Nav';
+import Content from './components/Content';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container-fluid">
-          <div className="row justify-content-end">
-            <div className="col-3 text-center bg-dark fixed-top navigation">
-                <Nav/>
+          <div className="row">
+            <div className="col-3 text-center bg-dark">
+              <Nav/>
             </div>
-            <div className="col-9 text-center">
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-              <h2 className="my-5">Contenido</h2>
-
-
+            <div className="col-9">
+              <Content/>
             </div>
           </div>
         </div>
---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Content = () => {
+    return(
+        <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+              qwdqwdqwdqwdqw
+            </div>
+            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+              qwdqwdqwdqwdqwdwqdqwdwqdwqdqwbjqdbj kdwqbjkdwqbdqwbjdwbjkqbjkdqwbjqdwbjkdqwbjwqdbjkwdqbjkdqwbdqwb kjdqwbjbdqwjbjwdkqbjdkqwbjwdqjbkdwqbjkwdqjbkdwbkjdwqbjkqwdbjkdwqbjdbjkdqwbjkdqwbjdwqbjqd kwjbdbjqwkbjdqwbqdwbjdqwjbkdq wbjkdqwbjkdqwbjdqwbjqdwbjkdqwbjkdqwjbqbkjdwqwbjdqwbjkqwddqwbjdqwbjkdqwbjdwqbjkdwbqjkqwbjkdbwjqdwbjkwkbdjjbwkdqjwqdbj kdwqbjkdwqbjkdqwbjkqwdbjkdqwbjkqdwbjkdqwbjkdqwbjkqwdbjqwjbdwqbjdqwbjkqwdbjkdqwbjkdwqbjkdqwbjdw qbjdqwbkjdqwbjkdqwbkj
+            </div>
+            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+              ..12312312312312312312.
+            </div>
+            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+              ..wefwr13r23fr23r23r2rf.
+            </div>
+        </div>
+    );
+}
+
+export default Content;
