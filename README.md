527c472c4506e9a99ecf1ae247a28f2fe20b496f
kabi404 <javierparada73@outlook.com>
2018-09-23 12:54:34 +0200

    add pre-build of skills

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # kabi404.com
 
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/kabi404/kabi404.github.io/blob/master/LICENSE)
-[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](http://makeapullrequest.com)
-[![CodeFactor](https://www.codefactor.io/repository/github/kabi404/kabi404.github.io/badge)](https://www.codefactor.io/repository/github/kabi404/kabi404.github.io)
 
 http://www.kabi404.me
 
--- src/components/content/subComponents/about/subcomponents/skills/Skills.js
+++ src/components/content/subComponents/about/subcomponents/skills/Skills.js
@@ -1,106 +1,120 @@
-import React from 'react';
+import React, {Component} from 'react';
+class Skills extends Component {
 
-const Skills = () => {
+    /*constructor(props) {
+        super(props);
 
-    var skills;
-    fetch('/data/skills.json')
-    .then(response => response.json())
-    .then(data => skills = data).catch((err)=>{
-        console.log(err);
-    });
+        this.state = {
+            skills : {}
+        }
+    }
 
-    console.log(skills);
+    loadSkills = () => {
+        fetch('/data/skills.json')
+            .then( (response) => {
+                return response.json()
+            })
+            .then( (data) => {
+                this.setState({
+                    skills: data
+                })
+        });
+    }*/
+    
+    render(){
+        //this.loadSkills();
 
-    return(
-        <div>
-            <div className="row justify-content-center mb-3">
-                <p className="h3 mt-5 text-center">My Skills {skills}</p>
-            </div>
-
-            <div className="row mb-4 justify-content-center">
-                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li className="nav-item">
-                        <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
-                            <i className="far fa-file-code"></i> Programming Languages
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
-                            <i className="fas fa-database"></i> DataBase
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">
-                            <i className="fas fa-screwdriver"></i> Other Technologies
-                        </a>
-                    </li>
-                </ul>
-            </div>
-                <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                    <div className="row mb-4 mx-3 my-2">
-                    <div className="col-sm-3">
-                        <div className="card bg-light">
-                            <div className="card-body">
-                                <p className="display-1 text-center">
-                                    <i className="fab fa-java"></i>
-                                </p>
-
-                                <p className="card-text text-center h5">
-                                    JAVA
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="card bg-light">
-                            <div className="card-body">
-                                <p className="display-1 text-center">
-                                    <i className="fab fa-js-square"></i>
-                                </p>
-
-                                <p className="card-text text-center h5">
-                                    JavaScript
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="card bg-light">
-                            <div className="card-body">
-                                <p className="display-1 text-center">
-                                    <i className="fab fa-php"></i>
-                                </p>
-
-                                <p className="card-text text-center h5">
-                                    PHP
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="card bg-light">
-                            <div className="card-body">
-                                <p className="display-1 text-center">
-                                    <i className="fab fa-python"></i>
-                                </p>
-
-                                <p className="card-text text-center h5">
-                                    Python
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+        return(
+            <div>
+                <div className="row justify-content-center mb-3">
+                    <p className="h3 mt-5 text-center">My Skills</p>
                 </div>
-                
-                </div>
-                <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-                    
-                
-                </div>
-                <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">loop</div>
 
-            </div>
-    );
+                <div className="row mb-4 justify-content-center">
+                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li className="nav-item">
+                            <a className="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">
+                                <i className="far fa-file-code"></i> Programming Languages
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
+                                <i className="fas fa-database"></i> DataBase
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">
+                                <i className="fas fa-screwdriver"></i> Other Technologies
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                    <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <div className="row mb-4 mx-3 my-2">
+                        <div className="col-sm-3">
+                            <div className="card bg-light">
+                                <div className="card-body">
+                                    <p className="display-1 text-center">
+                                        <i className="fab fa-java"></i>
+                                    </p>
+
+                                    <p className="card-text text-center h5">
+                                        JAVA
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card bg-light">
+                                <div className="card-body">
+                                    <p className="display-1 text-center">
+                                        <i className="fab fa-js-square"></i>
+                                    </p>
+
+                                    <p className="card-text text-center h5">
+                                        JavaScript
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card bg-light">
+                                <div className="card-body">
+                                    <p className="display-1 text-center">
+                                        <i className="fab fa-php"></i>
+                                    </p>
+
+                                    <p className="card-text text-center h5">
+                                        PHP
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card bg-light">
+                                <div className="card-body">
+                                    <p className="display-1 text-center">
+                                        <i className="fab fa-python"></i>
+                                    </p>
+
+                                    <p className="card-text text-center h5">
+                                        Python
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+
+
+                    </div>
+                    <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">loop</div>
+
+                </div>
+        )
+    };
 }
 
 export default Skills;
