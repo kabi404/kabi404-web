534d42ca64df8d0d083f727029dfc1d5ac171a7f
kabi404 <javierparada73@outlook.com>
2018-09-28 17:00:39 +0200

    add basic contents components

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,32 @@
 <template>
+  <div>
+    <div class="row justify-content-center">
+        <p class="display-4 mt-5 text-center">About me</p>
+    </div>
+    <div class="row justify-content-center">
+        <p class="h1 display-4 ">
+            <i class="fas fa-user-astronaut"></i>
+        </p>
+    </div>
+    
+    <div class="row justify-content-center">
+        <p class="h3 mt-5 text-center">Who am I</p>
+    </div>
+    <div class="row justify-content-center">
+        <p class="text-center my-2">
+            <span role="img" aria-label="pc">💻</span> 
+                &nbsp;I am a Software Engineering student at University of Málaga, Spain.&nbsp;
+            <span role="img" aria-label="pc">💻</span><br/>
+            <span role="img" aria-label="japanese-flags">🎌</span>
+                &nbsp;日本語の学生&nbsp;
+            <span role="img" aria-label="japanese-flags">🎌</span><br/>
+            <span role="img" aria-label="fox">🦊</span>
+                &nbsp;Fox lover&nbsp;
+            <span role="img" aria-label="fox">🦊</span>
+        </p>
+    </div>       
 
+  </div>
 </template>
 
 <script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,38 @@
 <template>
 
+  <div>
+    <div class="row justify-content-center">
+      <h1 class="display-4 mt-5 text-center">Javier Parada Tallante</h1>
+    </div>
+
+      <div class="row justify-content-center">
+          <h2 class="">kabi404</h2>
+      </div>
+      <div class="row justify-content-center">
+          <p class="h1 display-1 my-5">
+              <i class="fab fa-firefox"></i>
+          </p>
+      </div>
+      <div class="row justify-content-center">
+          <p class="text-center my-5">
+              <span role="img" aria-label="pc">💻</span> 
+                  &nbsp;Software Engineer&nbsp;
+              <span role="img" aria-label="pc">💻</span><br/>
+              <span role="img" aria-label="japanese-flags">🎌</span>
+                  &nbsp;日本語の学生&nbsp;
+              <span role="img" aria-label="japanese-flags">🎌</span><br/>
+              <span role="img" aria-label="telescope">🔭</span>
+                  &nbsp;Heir of Principia Mathematica&nbsp;
+              <span role="img" aria-label="telescope">🔭</span><br/>
+              <span role="img" aria-label="fox">🦊</span>
+                  &nbsp;I'm not human, I'm a proud fox&nbsp;
+              <span role="img" aria-label="fox">🦊</span>
+          </p>
+          
+      </div>
+
+  </div>
+
 </template>
 
 <script>
