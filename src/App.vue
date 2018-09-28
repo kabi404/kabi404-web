1396379c2d546465e2b2a00780e0ffacb43d45f7
Javier Parada Tallante <javierparada73@outlook.com>
2018-09-28 14:13:42 +0200

    add footer component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+
+    <Nav/>
+
+    <Content/>
+
+    <Footer/>
+
+  </div>
+</template>
+
+<script>
+
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
+
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,14 @@
+<template>
+
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-sm-2 text-center bg-dark">   
+          <Nav/>
+        </div>
+        <div class="col-10">
+          <Content/>
+        </div>
+    </div>
+</div>
+
+</template>
