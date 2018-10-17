6405fba8ea5c47735119497772067bff2eb63805
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-17 18:29:43 +0200

    fix height of hero

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var vue = new Vue({
     el: 'body',
     data: {
-
+        sections: [
+            {
+                title: 'About me',
+                icon: 'fas fa-user',
+            },
+            {
+                title: 'Skills',
+                icon: 'fas fa-brain',
+            },
+            {
+                title: 'Projects',
+                icon: 'fas fa-drafting-compass',               
+            },
+            {
+                title: 'Blog',
+                icon: 'fas fa-feather-alt',
+            },
+            {
+                title: 'Gallery',
+                icon: 'fas fa-camera-retro',
+            }
+        ]
     }
-})
+});
