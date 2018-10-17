5361ddab11d9f1135e0287ca50b0e4a99232320e
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-17 19:49:35 +0200

    add empty components and toggle tabs

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 var vue = new Vue({
-    el: 'body',
+    el: '#app',
     data: {
         sections: [
             {
                 title: 'About me',
                 icon: 'fas fa-user',
+                isActive: true,
             },
             {
                 title: 'Skills',
                 icon: 'fas fa-brain',
+                isActive: false,
             },
             {
                 title: 'Projects',
-                icon: 'fas fa-drafting-compass',               
+                icon: 'fas fa-drafting-compass',
+                isActive: false,            
             },
             {
                 title: 'Blog',
                 icon: 'fas fa-feather-alt',
+                isActive: false,
             },
             {
                 title: 'Gallery',
                 icon: 'fas fa-camera-retro',
+                isActive: false,
             }
         ]
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+        activeTab: function(index){
+            var sections = this.$data.sections;
+            for(var i=0; i<sections.length; i++)
+                sections[i].isActive = false;
+
+            sections[index].isActive = true;
+        }
     }
 });
