4916e3bfdaba4bcd80d94e84ad3e4c30c190992c
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-19 00:43:36 +0200

    add img

--- components/aboutme.js
+++ components/aboutme.js
@@ -29,7 +29,7 @@ var aboutme = Vue.component('aboutme', {
 
     <div>
         <div class="container">
-            <div class="columns is-centered m-t-xl m-b-xl">
+            <div class="columns is-7 is-variable is-centered m-t-xl">
                 <div class="column is-4">
                     <figure class="image is-3by4">
                         <img src="./img/wilson.png">
@@ -43,7 +43,7 @@ var aboutme = Vue.component('aboutme', {
                 </div>
             </div>
 
-            <div class="columns is-centered m-t-xxl m-b-xl">
+            <div class="columns is-centered is-7 is-variable m-t-xxl">
                 <div class="column is-6 is-vertical-center">
                     <div>
                         <p class="is-size-2 has-text-left-tablet">About me</p>
@@ -54,6 +54,20 @@ var aboutme = Vue.component('aboutme', {
                     <figure class="image is-1by1">
                         <img src="./img/mozilla.png">
                     </figure>
+                </div>
+            </div>
+
+            <div class="columns is-7 is-variable is-centered m-t-xxl">
+                <div class="column is-4">
+                    <figure class="image is-3by4">
+                        <img src="./img/nihon.jpg">
+                    </figure>
+                </div>
+                <div class="column is-6 is-vertical-center">
+                    <div>
+                        <p class="is-size-2 has-text-right-tablet">About me</p>
+                        <p class="is-size-5 has-text-justified">{{text.description[0]}}</p>
+                    </div>
                 </div>
             </div>
         </div>
