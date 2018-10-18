54180d1f392b2031e41530a08e228eeeb9a1545b
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-18 18:53:36 +0200

    add basic description and text-align

--- components/aboutme.js
+++ components/aboutme.js
@@ -1,7 +1,15 @@
 var aboutme = Vue.component('aboutme', {
     
     data: function(){
-
+        return {
+            text: {
+                description: [
+                    'Hola, Soy Javi! Soy estudiante de Ingeniería de Software en la Universidad de Málaga.',
+                    '',
+                    ''
+                ]
+            }
+        }
     },
 
     computed: {
@@ -17,22 +25,25 @@ var aboutme = Vue.component('aboutme', {
     },
 
     template:
-    `
+`
 
 <div>
-    <div class="container">
-        <div class="columns">
-            <div class="column is-4">
-                <figure class="image is-3by4">
-                    <img src="./img/wilson.png">
-                </figure>
-            </div>
-            <div class="column is-6">
-            
+        <div class="container">
+            <div class="columns is-centered m-t-xl m-b-xl">
+                <div class="column is-4">
+                    <figure class="image is-3by4">
+                        <img src="./img/wilson.png">
+                    </figure>
+                </div>
+                <div class="column is-6 is-vertical-center">
+                    <div>
+                        <p class="is-size-2 has-text-right-tablet">About me</p>
+                        <p class="is-size-5 has-text-justified">{{text.description[0]}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-    `
+`
 });
