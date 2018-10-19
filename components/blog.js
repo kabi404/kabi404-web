987e8e3e408f082f5041aaf064d9a983e7208903
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-19 19:00:27 +0200

    add all templates without rendering

--- components/blog.js
+++ components/blog.js
@@ -1,3 +1,31 @@
 var blog = Vue.component('blog', {
     
+    data: function(){
+        return {
+            text: {
+                
+            }
+        }
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    },
+
+    filters: {
+
+    },
+
+    template:
+`
+
+    <div>
+        <p class="has-text-centered is-size-2 m-t-xl m-b-xl">Empty Blog</p>
+    </div>
+
+`
 });
--- components/projects.js
+++ components/projects.js
@@ -27,19 +27,53 @@ var projects = Vue.component('projects', {
         <p class="has-text-centered is-size-2 m-t-xl m-b-xl">My Projects</p>
         <div class="columns is-centered">
             <div class="column is-3">
-                <figure class="image is-1by1">
-                    <img src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                      <div class="content">
+                        <p class="title has-text-centered">
+                            <i class="fas fa-terminal"></i>
+                        </p>
+                        <p class="subtitle">With even more content</p>
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
             </div>
+
+
             <div class="column is-3">
-                <figure class="image is-1by1">
-                    <img src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-            </div>
+                    <div class="tile is-parent">
+                        <article class="tile is-child box">
+                          <div class="content">
+                            <p class="title has-text-centered">
+                                <i class="fas fa-laptop-code"></i>
+                            </p>
+                            <p class="subtitle">With even more content</p>
+                            <div class="content">
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                          </div>
+                        </article>
+                    </div>
+                </div>
+
+
             <div class="column is-3">
-                <figure class="image is-1by1">
-                    <img src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                      <div class="content">
+                        <p class="title has-text-centered">
+                            <i class="fas fa-language"></i>
+                        </p>
+                        <p class="subtitle">With even more content</p>
+                        <div class="content">
+                          <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
             </div>
         </div>
     </div>
