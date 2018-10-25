aab34f4539c449c03d356fb4fa7ea2b861b10bb2
Javier Parada Tallante <javierparada73@outlook.com>
2018-10-25 13:40:07 +0200

    add language

--- components/skills.js
+++ components/skills.js
@@ -3,21 +3,31 @@ var skills = Vue.component('skills', {
     data: function(){
         return {
             text: {
-                techs: [
-                    ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF'],
-                    ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF']
-                    ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF'],
-                ],
-                programmingLanguages: [
-                    ['JAVA', 'JavaScript', 'C#', 'PHP', 'C'],
-                    ['JAVA', 'JavaScript', 'C#', 'PHP', 'C']
-                    ['JAVA', 'JavaScript', 'C#', 'PHP', 'C'],
-                ],
-                languages: [
-                    ['Español', 'Inglés', 'Japonés'],
-                    ['Spanish', 'English', 'Japanese'],
-                    ['スペイン語', '英語', '日本語']
-                ]
+                h1: ['Mis Habilidades', 'My Skills', ''],
+                techs: {
+                    title: ['Tecnologías', 'Technologies', ''],
+                    items: [
+                        ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF'],
+                        ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF'],
+                        ['Node.js', 'Electron', 'Vue.js', 'Bootstrap', 'Bulma', 'JSP', 'JSF']
+                    ]
+                },
+                code: {
+                    title: ['Código', 'Code', ''],
+                    items: [
+                        ['JAVA', 'JavaScript', 'C#', 'PHP', 'C', 'MongoDB', 'MySQL'],
+                        ['JAVA', 'JavaScript', 'C#', 'PHP', 'C', 'MongoDB', 'MySQL'],
+                        ['JAVA', 'JavaScript', 'C#', 'PHP', 'C', 'MongoDB', 'MySQL']
+                    ]
+                },
+                languages: {
+                    title: ['Idiomas', 'Languages', ''],
+                    items: [
+                        ['Español', 'Inglés', 'Japonés', 'Euskera'],
+                        ['Spanish', 'English', 'Japanese', 'Euskera'],
+                        ['スペイン語', '英語', '日本語', 'エウスケラ']
+                    ]
+                }
             }
         }
     },
@@ -39,7 +49,7 @@ var skills = Vue.component('skills', {
 
     <div>
 
-        <p class="has-text-centered is-size-2 m-t-xl">My skills</p>
+        <p class="has-text-centered is-size-2 m-t-xl">{{text.h1[0]}}</p>
 
         <div class="columns is-centered m-t-xs">
     
@@ -50,10 +60,10 @@ var skills = Vue.component('skills', {
                         <p class="title has-text-centered">
                             <i class="fas fa-terminal"></i>
                         </p>
-                        <p class="subtitle has-text-centered">Technologies</p>
+                        <p class="subtitle has-text-centered">{{text.techs.title[0]}}</p>
                         <hr/>
                         <div class="content has-text-centered">
-                          <p v-for="tech in text.techs[0]">{{tech}}</p>
+                          <p v-for="tech in text.techs.items[0]">{{tech}}</p>
                         </div>
                       </div>
                     </article>
@@ -68,11 +78,11 @@ var skills = Vue.component('skills', {
                             <p class="title has-text-centered">
                                 <i class="fas fa-laptop-code"></i>
                             </p>
-                            <p class="subtitle has-text-centered">Programming Languages</p>
+                            <p class="subtitle has-text-centered">{{text.code.title[0]}}</p>
                             <hr/>
                             <div class="content">
                                 <div class="content has-text-centered">
-                                    <p v-for="lan in text.programmingLanguages[0]">{{lan}}</p>
+                                    <p v-for="lan in text.code.items[0]">{{lan}}</p>
                                 </div>
                             </div>
                           </div>
@@ -88,11 +98,11 @@ var skills = Vue.component('skills', {
                         <p class="title has-text-centered">
                             <i class="fas fa-language"></i>
                         </p>
-                        <p class="subtitle has-text-centered">Languages</p>
+                        <p class="subtitle has-text-centered">{{text.languages.title[0]}}</p>
                         <hr/>
                         <div class="content">
                             <div class="content has-text-centered">
-                                <p v-for="lan in text.languages[0]">{{lan}}</p>
+                                <p v-for="lan in text.languages.items[0]">{{lan}}</p>
                             </div>
                         </div>
                       </div>
