d1ff9482a64af71414b6fb65f5a6da5708832988
Javier Parada Tallante <javierparada73@outlook.com>
2018-12-10 20:36:56 +0100

    v5.0.0

--- index.js
+++ index.js
@@ -1,63 +1,8 @@
-var vue = new Vue({
-    el: '#app',
-
-    created: function(){
-        var userLang = navigator.language || navigator.userLanguage;
-
-        if(userLang.includes('es')) this.$data.lan = 0;
-
-        if(userLang.includes('ja')) this.$data.lan = 2;
-    },
-
-    data: {
-        lan: 1,
-
-        text: {
-            name: ['Javier Parada Tallante', 'Javier Parada Tallante', 'ハビエル・パラダタ・ジャンテ'],
-            job: ['Software Engineer', 'Software Engineer', 'ソフトウェアエンジニア']
-        },
-
-        sections: [
-            {
-                title: 'About me',
-                icon: 'fas fa-user',
-                isActive: true,
-            },
-            {
-                title: 'Skills',
-                icon: 'fas fa-brain',
-                isActive: false,
-            },
-            {
-                title: 'Projects',
-                icon: 'fas fa-drafting-compass',
-                isActive: false,            
-            },
-            {
-                title: 'Blog',
-                icon: 'fas fa-feather-alt',
-                isActive: false,
-            },
-            {
-                title: 'Gallery',
-                icon: 'fas fa-camera-retro',
-                isActive: false,
-            }
-        ]
-    },
-
-    computed: {
-
-    },
-
-    methods: {
-        activeTab: function(index){
-            var sections = this.$data.sections;
-            for(var i=0; i<sections.length; i++)
-                sections[i].isActive = false;
-
-            sections[index].isActive = true;
-        }
-    }
-});
-
+(function() {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#'+burger.dataset.target);
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
