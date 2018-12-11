7f7d8b53f1fc88ae22fc26204c0d25c16452ec30
Javier Parada Tallante <javierparada73@outlook.com>
2018-12-11 23:18:44 +0100

    add lang with vue.js

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
-(function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+var app = new Vue({
+    el: '#app',
+    data: {
+        lang: {},
+        currentLang: ''
+    },
+
+    created: function () {
+        if(navigator.language.includes('es'))
+        {
+            this.$data.currentLang = 'es';
+        } else {
+            this.$data.currentLang = 'en';
+        }
+            
+        //else if(navigator.language.contains('jp'))
+        //this.$data.currentLang = 'jp';      
+
+        this.getLangData();
+    },
+
+    methods: {
+        getLangData: function() {
+            let $this = this;
+            var xobj = new XMLHttpRequest();
+            xobj.overrideMimeType("application/json");
+            xobj.open('GET', './data/lang.json', true);
+            xobj.onreadystatechange = function () {
+                if (xobj.readyState == 4 && xobj.status == "200") {
+                    $this.$data.lang = JSON.parse(xobj.responseText);
+                }
+            };
+            xobj.send();
+        },
+
+        langify: function(name) {
+            let data;
+            try {
+                data = this.$data.lang[name][this.$data.currentLang];
+            } catch(err) {
+                data = ''
+            }
+            return data;
+        }
+    },
+
+    computed: {
+        
+    }
+})
