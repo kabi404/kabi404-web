8fa049bca3bd9508c40490f06de274d881819840
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-25 20:40:49 +0200

    Add language support

---
+++ main.js
@@ -0,0 +1,65 @@
+var app = new Vue({
+    el: '#app',
+
+    data: {
+      test: 'Test Vue!',
+
+      currentLan: 'en',
+
+      content: {
+          JAVIER_PARADA: {
+            es: 'Javier Parada',
+            en: 'Javier Parada',
+            jp: 'パラだ葉火'
+          },
+          SOFTWARE_ENGINEER: {
+            es: 'Ingeniero de Software y Desarrollador Full-stack ',
+            en: 'Software Engineer and Full-Stack Developer',
+            jp: 'ソフトウェアエンジニアとフルスタック開発者'
+          },
+          PAGE_HOSTED_ON_GITHUB: {
+            es: 'Página alojada en Github',
+            en: 'Web hosted on Github',
+            jp: 'GithubでホストされているWeb'
+          },
+          HOME: {
+            es: 'Inicio',
+            en: 'Home',
+            jp: ''
+          },
+          SKILLS: {
+            es: 'Habilidades',
+            en: 'Skills',
+            jp: ''
+          },
+          PROJECTS: {
+            es: 'Projects',
+            en: 'Web hosted on Github',
+            jp: ''
+          },
+          LANGUAGE: {
+            es: 'Language',
+            en: 'Web hosted on Github',
+            jp: ''
+          }
+      }
+    },
+
+    methods: {
+        lang: function(strId) {
+            return this.content[strId][this.currentLan]
+        }
+    },
+
+    beforeMount: function() {
+        let userLang = navigator.language || navigator.userLanguage
+        if(userLang.includes('es')) {
+            this.currentLan = 'es'
+        } else if(userLang.includes('jp')) {
+            this.currentLan = 'jp'
+        } else {
+            this.currentLan = 'en'
+        }
+    }
+
+  })
