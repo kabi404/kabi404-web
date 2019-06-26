9617afd6d730dfddc98e7f469cab902d18a3820e
Javier Parada Tallante <javierparada73@outlook.com>
2019-06-26 09:08:25 +0200

    add burger toggle

--- main.js
+++ main.js
@@ -48,6 +48,23 @@ var app = new Vue({
     methods: {
         lang: function(strId) {
             return this.content[strId][this.currentLan]
+        },
+
+        burgerToggleEventAdd: function() {
+          const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+          if ($navbarBurgers.length > 0) {
+            $navbarBurgers.forEach( el => {
+              el.addEventListener('click', () => {
+              
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+    
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+              });
+            });
+          }
         }
     },
 
@@ -60,6 +77,10 @@ var app = new Vue({
         } else {
             this.currentLan = 'en'
         }
+    },
+
+    mounted: function() {
+      this.burgerToggleEventAdd()
     }
 
   })
