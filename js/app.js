b3ea7d7c7b417d56f4e92f73f662023842e9b569
Javier Parada Tallante <javierparada73@outlook.com>
2019-07-09 16:58:39 +0200

    Add projects as component

--- js/app.js
+++ js/app.js
@@ -42,77 +42,11 @@ var app = new Vue({
             en: 'Close',
             jp: '言語'
           },
-          PROJECTS_CARDS: {
-            es: [
-              {
-                title: 'Kanjinary',
-                subtitle: 'Javier Parada',
-                description: 'Aplicación web y base de datos para estudiar kanjis japoneses en el orden de estudio japonés. Datos obtenidos de apuntes de la Escuela Oficial de Idiomas de Málaga.',
-                projectImg: './img/kanjinary.png',
-                orgImg: 'https://avatars3.githubusercontent.com/u/19194763?s=460&v=4',
-                downloadLink: 'https://www.kabi404.dev/kanjinary/',
-                repoLink: 'https://github.com/kabi404/kanjinary'
-              },{
-                title: 'PPCE',
-                subtitle: 'Javier Parada',
-                description: 'Plataforma de Prevención y Control de Emergencias. Trabajo de final de grado en la Universidad de Málaga en el grado de Ingeniería del Software.',
-                projectImg: './img/ppce.png',
-                orgImg: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Universidad_de_Málaga.jpg',
-                downloadLink: 'http://www.kabi404.dev/PPCE/',
-                repoLink: 'https://github.com/kabi404/PPCE'
-              },{
-                title: 'Kanami',
-                subtitle: 'Javier Parada',
-                description: 'Aplicación web y de escritorio minimalista para aprender los silabarios japoneses Hiragana y Katakana. Una sola compilación para generar ambas aplicaciones.',
-                projectImg: './img/kanami.png',
-                orgImg: 'https://avatars3.githubusercontent.com/u/19194763?s=460&v=4',
-                downloadLink: 'https://www.kabi404.dev/Kanami/',
-                repoLink: 'https://github.com/kabi404/Kanami'
-              },{
-                title: 'Mikana',
-                subtitle: 'ArtichokeCore',
-                description: 'Aplicación móvil nativa Android para aprender los silabarios japoneses Hiragana y Katakana.',
-                projectImg: './img/mikana.png',
-                orgImg: 'https://avatars2.githubusercontent.com/u/35332682?s=200&v=4',
-                downloadLink: 'https://play.google.com/store/apps/details?id=com.artichokecore.mikana&hl=es',
-                repoLink: 'https://github.com/kabi404/Kanami'
-              },{
-                title: 'Joseto',
-                subtitle: 'Javier Parada',
-                description: 'Bot de Discord modular al que introducirle facilmente comandos y diversas funcionalidades.',
-                projectImg: './img/joseto.png',
-                orgImg: 'https://avatars3.githubusercontent.com/u/19194763?s=460&v=4',
-                downloadLink: 'https://www.kabi404.dev/Joseto/',
-                repoLink: 'https://github.com/kabi404/Joseto'
-              }
-            ],
-            en: [
-              {
-                title: 'Kanami',
-                subtitle: 'Javier Parada',
-                description: 'Esto es la descripcion',
-                projectImg: './img/kanami.png',
-                orgImg: 'https://avatars3.githubusercontent.com/u/19194763?s=460&v=4',
-                downloadLink: 'https://www.kabi404.dev/Kanami/',
-                repoLink: 'https://github.com/kabi404/Kanami'
-              }
-            ],
-            jp: [
-              {
-                title: 'Kanami',
-                subtitle: 'Javier Parada',
-                description: 'Esto es la descripcion',
-                projectImg: './img/kanami.png',
-                orgImg: 'https://avatars3.githubusercontent.com/u/19194763?s=460&v=4',
-                downloadLink: 'https://www.kabi404.dev/Kanami/',
-                repoLink: 'https://github.com/kabi404/Kanami'
-              }
-            ]
-          }
       }
     },
 
     components: {
+      projects,
       skills,
       home
     },
