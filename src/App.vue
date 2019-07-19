47b2898cbc2accf8da760556ac0662c6a88e0f4e
Javier Parada Tallante <javierparada73@outlook.com>
2019-07-19 09:14:47 +0200

    Add home

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,8 @@
                 <Navbar/>
             </div>
             <div class="hero-body">
-
                 <router-view></router-view>
-
-                <!-- Languages -->
-                <div class="modal" v-bind:class="{'is-active': showLanModal}">
-                  <div class="modal-background"></div>
-                  <div class="modal-card">
-                    <header class="modal-card-head">
-                      <p class="modal-card-title">{{lang('LANGUAGE')}}</p>
-                      <button @click="toggleLanguageModal()" class="delete" aria-label="close"></button>
-                    </header>
-                    <section class="modal-card-body has-text-centered">
-                        <div class="control">
-                            <div class="columns">
-                                <div class="column">
-                                    <a @click="setLan('es')">
-                                        <div v-bind:class="{'has-background-grey-dark has-text-white': currentLan == 'es'}" class="box has-text-centered">
-                                            <p>Español</p>
-                                            <p>Spanish</p>
-                                            <p>スペイン語</p>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="column">
-                                    <a @click="setLan('en')">
-                                        <div v-bind:class="{'has-background-grey-dark has-text-white': currentLan == 'en'}" class="box has-text-centered">
-                                            <p>Inglés</p>
-                                            <p>English</p>
-                                            <p>英語</p>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="column">
-                                    <a @click="setLan('jp')">
-                                        <div v-bind:class="{'has-background-grey-dark has-text-white': currentLan == 'jp'}" class="box has-text-centered">
-                                            <p>Japonés</p>
-                                            <p>Japanese</p>
-                                            <p>日本語</p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <footer class="modal-card-foot">
-                      <button @click="toggleLanguageModal()" class="button">{{lang('CLOSE')}}</button>
-                    </footer>
-                  </div>
-                </div>
-                <!-- end Language -->
+                <LanModal v-if="showLanModal"/>
             </div>
           <Footer/>
         </section>
@@ -64,6 +16,7 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import LanModal from './components/Language.vue'
 
 export default {
   name: 'app',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,68 @@
 <template>
-  <div class="home">
-    holaaa
+    <div class="container has-text-centered">
+      <div class="columns is-vcentered is-centered">
+          <div class="column is-3">
+              <figure class="image is-1by1">
+                  <img src="https://avatars3.githubusercontent.com/u/19194763?s=460&v=4"
+                      class="is-rounded" alt="Description">
+              </figure>
+          </div>
+          <div class="column is-6 is-offset-1">
+              <h1 id="typeit" class="title is-2">
+                  {{ lang('JAVIER_PARADA') }}
+              </h1>
+              <h2 class="subtitle is-4">
+                  {{ lang('SOFTWARE_ENGINEER') }}
+              </h2>
+              <br>
+              <div class="columns is-centered is-multiline has-text-centered">
+                  <div class="column is-2">
+                      <a target="_blank" href="https://github.com/kabi404"
+                          class="is-size-3 has-text-grey-dark">
+                          <i class="fab fa-github"></i>
+                      </a>
+                  </div>
+                  <div class="column is-2">
+                      <a target="_blank" href="https://www.linkedin.com/in/kabi404/"
+                          class="is-size-3 has-text-grey-dark">
+                          <i class="fab fa-linkedin-in"></i>
+                      </a>
+                  </div>
+                  <div class="column is-2">
+                      <a target="_blank" href="https://twitter.com/kabi404"
+                          class="is-size-3 has-text-grey-dark">
+                          <i class="fab fa-twitter"></i>
+                      </a>
+                  </div>
+                  <div class="column is-2">
+                      <a href="mailto:kabi404@outlook.com" class="is-size-3 has-text-grey-dark">
+                          <i class="fas fa-envelope"></i>
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'home',
-  components: {
 
+  methods: {
+    lang: function (strId) {
+      return 'miau'
+    }
+  },
+
+  computed: {
+    currentTab: function () {
+      return this.$route.name
+    },
+
+    currentLan: function() {
+      return this.$store.state.lan
+    }
   }
 }
 </script>
