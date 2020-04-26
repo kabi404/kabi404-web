a8e3d3cc310d6c34102cc5d7512f90a443e7083f
Javier Parada <javierparada73@outlook.com>
2020-04-26 20:20:34 +0200

    Delete all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Habinari web
-
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-
-gulp.task('styles', async () => gulp
-  .src('./styles/*.scss')
-    .pipe(sass())
-  .pipe(gulp.dest('./css')))
